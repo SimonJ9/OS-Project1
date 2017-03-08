6e2d1ac0de62d998944a5cb44cc4cce3bd837e25
Chenjun Zhou <zcj0709@gmail.com>
2017-03-09 07:32:07 +0800

    wrong wait time for some test cases

--- SRT.h
+++ SRT.h
@@ -40,13 +40,24 @@ void sim_SRT(const struct proc_queue* q, const unsigned int t_cs)
     int switchin = 0;
     int switchout = 0;
     
+    int cs = 0; /* context switches */
+    int pp = 0; /* preemptions */
+    float cpu = 0; /* CPU burst time */
+    int burst_num = 0;
+    float wait = 0; /* wait time */
+    float turnaround = 0; /* turnaround time */
+    
+    
     int addq = 0;
     
     while(stillworking)
     {
+        
+        
         int i;
-
+        
         if ((&bqueue)->_queue != NULL){
+
             /* preempt occur */
             if ((&rqueue)->_queue != NULL && ((&bqueue)->_queue[0].burst_countdown > (&rqueue)->_queue[0]._t_burst || 
                     ((&rqueue)->_queue[0].burst_countdown > 0 && (&bqueue)->_queue[0].burst_countdown > (&rqueue)->_queue[0].burst_countdown))){
@@ -54,6 +65,7 @@ void sim_SRT(const struct proc_queue* q, const unsigned int t_cs)
                 /* switchout time set */
                 if (switchout == 0) {
                     switchout = 1;
+                    cs++;
                     (&bqueue)->_queue[0].switch_out_countdown = t_cs/2-1;
                 }/* switchout time finish */
                 else if ((&bqueue)->_queue[0].switch_out_countdown != 0){
@@ -88,9 +100,11 @@ void sim_SRT(const struct proc_queue* q, const unsigned int t_cs)
                         else {
                             printf("time %dms: Process %c terminated %s\n",current_time, (&bqueue)->_queue[0]._PID, queue_status(&rqueue));
                             fflush(stdout);
+                            turnaround = turnaround + current_time - (&bqueue)->_queue[0]._t_arrival + 3;
                         }
                         switchout = 1;
-                        (&bqueue)->_queue[0].switch_out_countdown = t_cs/2;
+                        cs++;
+                        (&bqueue)->_queue[0].switch_out_countdown = t_cs/2; 
                         
                         
                     }/* switchout time finish */
@@ -119,8 +133,7 @@ void sim_SRT(const struct proc_queue* q, const unsigned int t_cs)
         }
         
         /*check io queue*/
-        for (i = 0; i < (&ioqueue)->_size && (&ioqueue)->_queue != NULL; i++){
-        
+        for (i = 0; i < (int)(&ioqueue)->_size && (&ioqueue)->_queue != NULL; i++){
             
             if ((&ioqueue)->_queue[i].io_countdown > 0 ) (&ioqueue)->_queue[i].io_countdown--;
             else{
@@ -131,13 +144,15 @@ void sim_SRT(const struct proc_queue* q, const unsigned int t_cs)
                     printf("time %dms: Process %c completed I/O and will preempt %c %s\n", current_time, (&ioqueue)->_queue[i]._PID, (&bqueue)->_queue[0]._PID, queue_status(&rqueue));
                     fflush(stdout);
                     
+                    pp++;
+                    
                     addq = add_process((&rqueue),(const struct process *) &((&ioqueue)->_queue[i]));
                     if (addq < 0) fprintf(stderr, "ERROR: add failed\n");
                     
                     remove_process(&ioqueue, &((&ioqueue)->_queue[i]));
                     /* if the old first one in ready queue are switching in */
                     if (switchin == 1) switchin = 0;
-                    if (i < (&ioqueue)->_size) i--;
+                    if (i < (int)(&ioqueue)->_size) i--;
                     qsort( (&rqueue)->_queue, (&rqueue)->_size, sizeof( struct process ), compare);
                 }
                 else{
@@ -149,21 +164,26 @@ void sim_SRT(const struct proc_queue* q, const unsigned int t_cs)
                     fflush(stdout);
                     
                     remove_process(&ioqueue, &((&ioqueue)->_queue[i]));
-                    if (i < (&ioqueue)->_size) i--;
+                    if (i < (int)(&ioqueue)->_size) i--;
                     
                 }
                 
             }
         }
          /*chck if any new process*/
-        
-        for (i = 0; i < q->_size; i++){
+        for (i = 0; i < (int)q->_size; i++){
             if (current_time == q->_queue[i]._t_arrival){
+                
+                cpu += (q->_queue[i]._t_burst) * (q->_queue[i]._n_burst);
+                burst_num += q->_queue[i]._n_burst;
+                turnaround -=  (q->_queue[i]._t_io) * (q->_queue[i]._n_burst - 1);
                 /* check if preempt occur */
                 if ((&bqueue)->_queue != NULL && check_preempt(&((&bqueue)->_queue[0]),&(q->_queue[i])) > 0 && switchout == 0) {
                                 
                     printf("time %dms: Process %c arrived and will preempt %c %s\n", current_time, q->_queue[i]._PID, (&bqueue)->_queue[0]._PID, queue_status(&rqueue));
                     fflush(stdout);
+                    
+                    pp++;
                     
                     addq = add_process((&rqueue),(const struct process *) &(q->_queue[i]));
                     if (addq < 0) fprintf(stderr, "ERROR: add failed\n");
@@ -183,6 +203,7 @@ void sim_SRT(const struct proc_queue* q, const unsigned int t_cs)
             }
         }
         
+
         /*No burst, but some ready*/
         if ((&bqueue)->_queue == NULL && (&rqueue)->_queue != NULL){
             
@@ -193,7 +214,6 @@ void sim_SRT(const struct proc_queue* q, const unsigned int t_cs)
             }/* switchin time finish */
             else if ((&rqueue)->_queue[0].switch_in_countdown != 0){
                 (&rqueue)->_queue[0].switch_in_countdown--;
-                
             }
             
             if (switchin == 1 && (&rqueue)->_queue[0].switch_in_countdown == 0)  {  
@@ -220,11 +240,21 @@ void sim_SRT(const struct proc_queue* q, const unsigned int t_cs)
             
         }
         
+        wait += (&rqueue)->_size;
+        if (switchin == 1 && (&rqueue)->_queue[0].switch_in_countdown > 0) wait--;
+        
+        
         current_time++;
     }
     
     
     current_time--;
+    
+    wait = wait/burst_num;
+    
+    cpu = cpu / burst_num;
+    turnaround = turnaround / burst_num;
+    
     
     
     printf("time %dms: Simulator ended for SRT %s\n", current_time, queue_status(&rqueue));
